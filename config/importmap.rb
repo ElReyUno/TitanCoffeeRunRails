af09238dfd28e333d065f1beab146f3fe2d7cd40
EL REY UNO <ezasw@gmail.com>
2025-07-06 18:18:08 -0400

    Migrate Frontend Assets (CSS, JS, images)

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,11 @@
 # Pin npm packages by running ./bin/importmap
+# Asset Pipeline Configuration
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "stimulus", to: "stimulus.min.js", preload: true
+pin "stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "turbo", to: "turbo.min.js", preload: true
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/titans", under: "titans"
