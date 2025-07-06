5af120982182ece4f78f991e5e078db45203f428
EL REY UNO <ezasw@gmail.com>
2025-07-06 18:22:47 -0400

    Implement Proper Authentication Flows

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email ])
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_sales_path
+    else
+      products_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    products_path
+  end
 end
